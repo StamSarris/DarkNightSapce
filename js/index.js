93a3f5009d1478728aa9b7b0879ff3686020e35c
StamSarris <stamsarris@gmail.com>
2020-12-09 12:57:05 +0200

    add nav slide

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('hello dude')
+console.log('test');
+//select the nav
+//add event click
+//addeffect
+
+const cl = document.querySelector('.navopen');
+
+
+cl.addEventListener('click', () => {
+    document.querySelector('.nav_mobile').style.display = 'flex';
+
+}
+);
+
+document.querySelector('.nav_mobile_title').addEventListener('click',()=>{
+    document.querySelector('.nav_mobile').style.display = 'none';
+   
+})
